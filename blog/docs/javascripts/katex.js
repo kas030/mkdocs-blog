eb746d45e08eaf9e368aa69254c18cfba3956b8d
kas030 <kas_1120@163.com>
2026-07-10 23:57:51 +0800

    feat: add katex macros for games101

--- blog/docs/javascripts/katex.js
+++ blog/docs/javascripts/katex.js
@@ -1,3 +1,18 @@
+const games101Macros = {
+  "\\vctwo": "\\begin{bmatrix}#1\\\\#2\\end{bmatrix}",
+  "\\vcthree": "\\begin{bmatrix}#1\\\\#2\\\\#3\\end{bmatrix}",
+  "\\vcfour": "\\begin{bmatrix}#1\\\\#2\\\\#3\\\\#4\\end{bmatrix}",
+  "\\mattwo": "\\begin{bmatrix}#1 & #2\\\\#3 & #4\\end{bmatrix}",
+  "\\matthree": "\\begin{bmatrix}#1\\\\#2\\\\#3\\end{bmatrix}",
+  "\\matfour": "\\begin{bmatrix}#1\\\\#2\\\\#3\\\\#4\\end{bmatrix}",
+  "\\ct": "\\cos\\theta",
+  "\\st": "\\sin\\theta",
+  "\\ca": "\\cos\\alpha",
+  "\\sa": "\\sin\\alpha"
+}
+
+const isGames101Page = () => location.pathname.includes("/courses/games101/")
+
 document$.subscribe(({ body }) => { 
   renderMathInElement(body, {
     delimiters: [
@@ -6,5 +21,6 @@ document$.subscribe(({ body }) => {
       { left: "\\(", right: "\\)", display: false },
       { left: "\\[", right: "\\]", display: true }
     ],
+    macros: isGames101Page() ? games101Macros : {},
   })
 })
